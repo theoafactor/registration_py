c23b8c6f7622e7e25d98affdb8b83a9d5434279a
Olu Adeyemo:Astronaut <theoafactor@gmail.com>
2023-03-09 11:29:57 +0100

    Added storage and file checks

--- user/save_user_info.py
+++ user/save_user_info.py
@@ -3,5 +3,13 @@ def saveUserInfo(user_info):
     ## check if the storage exists already..
     ## the storage will be saved to the users.txt in the 'storage' folder
     ## check if users.txt file exists
-    
+    import os.path ## <--- What does this module do?
+
+    if os.path.isdir("storage") and os.path.isfile("storage/users.py"):
+        print("You may continue")
+    else:
+        if not os.path.isdir("storage"):
+            print("the storage does not exist")
+        if not os.path.isfile("storage/users.py"):
+            print("the file does not exist")
 
